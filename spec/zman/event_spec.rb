7c22e80acddc5eb6c36d6123f6fdba7a547fcacc
Delon R. Newman <contact@delonnewman.name>
2024-11-15 22:51:17 -0500

    Add test for composing dates in events

--- spec/zman/event_spec.rb
+++ spec/zman/event_spec.rb
@@ -1,4 +1,12 @@
 require_relative '../../lib/zman'
 
 RSpec.describe Zman::Event do
+  it 'composes date objects' do
+    date = Zman::Date.new(-1513, 1, precision: :circa)
+    exodus = described_class.new(title: 'Exodus', date:)
+
+    expect(exodus.date_value).to eq(date.value)
+    expect(exodus.date_precision_value).to eq(date.precision_value)
+    expect(exodus.date).to eq(date)
+  end
 end
