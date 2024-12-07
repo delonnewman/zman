b542ab0beae177ab18b04345cecde76b7c7269b7
Delon R. Newman <contact@delonnewman.name>
2024-12-06 21:03:01 -0500

    Add date range logic

--- lib/zman/event.rb
+++ lib/zman/event.rb
@@ -3,7 +3,28 @@
 module Zman
   class Event < Entity
     has :title, String
-    composite :date, of: { value: Integer, precision_value: Integer }, class_name: 'Zman::Date', constructor: :composite
+    composite :start_on, of: { value: Integer, precision: Integer }, to: Zman::Date
+    composite :end_on, of: { value: Integer, precision: Integer }, to: Zman::Date, optional: true
     timestamp
+
+    def date
+      return if range?
+
+      start_on
+    end
+
+    def date_range
+      return if single?
+
+      start_on..end_on
+    end
+
+    def single?
+      !range?
+    end
+
+    def range?
+      start_on && end_on
+    end
   end
 end
